33bf77117353f425dc31690d818f03107ced727b
Gerard <grdllado@gmail.com>
2017-03-06 18:12:10 +0100

    Initial commit

---
+++ src/org/escoladeltreball/libgdx/demo1/iam47116609/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.escoladeltreball.libgdx.demo1.iam47116609;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.escoladeltreball.libgdx.demo1.iam47116609.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
